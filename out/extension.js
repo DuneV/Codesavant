2c966abb0f3d215d70655b3394dccf837fa58247
Daniel Felipe Triviño <d.trivino@uniandes.edu.co>
2023-09-03 21:01:45 -0500

    output files

--- out/extension.js
+++ out/extension.js
@@ -4,39 +4,44 @@ exports.deactivate = exports.activate = void 0;
 const vscode = require("vscode");
 const axios_1 = require("axios");
 function activate(context) {
-    let disposable = vscode.commands.registerCommand('test-extension.sendRequestToServer', async () => {
+    let sendRequestToServer = vscode.commands.registerCommand("intelliuml.sendRequestToServer", async () => {
         try {
             const activeEditor = vscode.window.activeTextEditor;
             if (!activeEditor) {
-                vscode.window.showInformationMessage('No estas viendo nada');
+                vscode.window.showErrorMessage("No hay ningún editor activado");
+                return;
+            }
+            else if (activeEditor.document.fileName.slice(-3) != "xmi") {
+                vscode.window.showErrorMessage("Debe subir un archivo .XMI");
                 return;
             }
             // Obtén el contenido del archivo abierto actualmente en el editor
             const document = activeEditor.document;
             const fileContent = document.getText();
             // Define la URL de tu servidor
-            const serverURL = 'http://localhost:3000/process-file';
+            const serverURL = "http://localhost:3000/process-file";
             // Realiza una solicitud POST al servidor con el contenido del archivo
             const response = await axios_1.default.post(serverURL, { content: fileContent });
+            console.log(response);
             // Si el servidor responde con éxito, muestra una notificación
             if (response.status === 200) {
-                vscode.window.showInformationMessage('Solicitud al servidor completada con éxito');
+                vscode.window.showInformationMessage("Solicitud al servidor completada con éxito");
             }
         }
         catch (error) {
             if (error instanceof Error) {
                 // Maneja errores de conexión o del servidor
-                vscode.window.showErrorMessage('Error al enviar la solicitud al servidor: ' + error.message);
+                vscode.window.showErrorMessage("Error al enviar la solicitud al servidor: " + error.message);
             }
         }
     });
-    let concept = vscode.commands.registerCommand('test-extension.helloWorld', () => {
+    let helloWorld = vscode.commands.registerCommand("intelliuml.helloWorld", () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from test-extension!');
+        vscode.window.showInformationMessage("Hello World from test-extension!");
     });
-    context.subscriptions.push(disposable);
-    context.subscriptions.push(concept);
+    context.subscriptions.push(sendRequestToServer);
+    context.subscriptions.push(helloWorld);
 }
 exports.activate = activate;
 // This method is called when your extension is deactivated
